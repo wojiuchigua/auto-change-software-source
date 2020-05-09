ddd150eb848c5df95573a769e79a80b0a086e49a
wojiuchigua <53941684+wojiuchigua@users.noreply.github.com>
2020-05-10 00:36:36 +0800

    Update change.sh
    update

--- change.sh
+++ change.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 #=========================================================
-#       System Required: Centos 6/7,Ubuntu 16/18,Debian 8+
+#       onautomationem Required: Centos 6/7,Ubuntu 16/18,Debian 8+
 #       Description: replace software source script
 #       Version: 1.0
 #       Author: wojiuchigua
@@ -11,7 +11,7 @@ export PATH
 #引用
 source /etc/os-release
 #变量
-sh_ver="1.0"
+sh_ver="2.0"
 
 dl="dl.lxw2004.xyz/linux"
 error="错误"
@@ -19,18 +19,21 @@ kanxi="但是我拒绝s"
 Error="错误"
 gen="read -p"请输入数字""
 none="请输入正确的数字"
-muban=" 
+tongyong="
+--------------------选择系统源----------------------------
+${Green_font_prefix}1.${Font_color_suffix} 阿里源
+${Green_font_prefix}2.${Font_color_suffix} 腾讯源
+${Green_font_prefix}3.${Font_color_suffix} 清华源
+${Green_font_prefix}4.${Font_color_suffix} 网易源
+${Green_font_prefix}5.${Font_color_suffix} 退出
+---------------------------------------------------------"
+onmuban="
 --------------------选择系统-----------------------------
  ${Green_font_prefix}1.${Font_color_suffix} ubuntu
  ${Green_font_prefix}2.${Font_color_suffix} centos
  ${Green_font_prefix}3.${Font_color_suffix} debian
- ${Green_font_prefix}4.${Font_color_suffix} openSUSE
- ${Green_font_prefix}5.${Font_color_suffix} fedora
- ${Green_font_prefix}6.${Font_color_suffix} arch
- ${Green_font_prefix}7.${Font_color_suffix} kali
- ${Green_font_prefix}8.${Font_color_suffix} deepin
- ${Green_font_prefix}9.${Font_color_suffix} alpine
- ${Green_font_prefix}10.${Font_color_suffix} 返回上级
+ ${Green_font_prefix}4.${Font_color_suffix} fedora
+ ${Green_font_prefix}5.${Font_color_suffix} 返回上级
 ---------------------------------------------------------"
 
 ubuntu1="
@@ -71,6 +74,7 @@ opensus="
  ${Green_font_prefix}5.${Font_color_suffix} 0.5
  ${Green_font_prefix}6.${Font_color_suffix}返回上级
 ---------------------------------------------------------"
+##这个源太多啦不干
 alpine="
 --------------------------------------------------------
  ${Green_font_prefix}1.${Font_color_suffix} 2.4
@@ -98,7 +102,7 @@ Update_shell(){
 	sh_new_ver=$(wget --no-check-certificate -qO- "http://${dl}/change.sh"|grep 'sh_ver="'|awk -F"=" ' {print $NF}' |sed 's/\"//g'|head -1)
         [[ -z ${sh_new_ver} ]] && echo -e "${kanxi} 检测失败" && menu
 	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
-	echo -e "发现新版本[${sh_new_ver}],是否更新?[Y/N]"	
+	echo -e "发现新版本[${sh_new_ver}],是否更新?[Y/N]"
         read -p "(默认Y):" yn
 	[[ -z "${yn}" ]] && yn="y"
 	if [[ ${yn} == [Yy] ]]; then
@@ -118,7 +122,7 @@ Update_shell(){
 
 #以下结为自动化
 #ubuntu
-automation(){
+onautomation(){
 if [ "${ID}" = "ubuntu" ];then
 yuan="ali"
 sys="${ID}"
@@ -352,475 +356,185 @@ else
 osu
 fi
 }
-#主菜单
-menu(){
+#选择离线在线与自动
+allselect(){
 clear
 echo && echo -e "换源脚本菜单 ${Red_font_prefix}[v${sh_ver}]}${Font_color_suffix}
          -- 换源脚本 | www.lxw2004.xyz --
- ${Green_font_prefix}0.${Font_color_suffix}升级脚本
- --------------------选择系统源----------------------------
- ${Green_font_prefix}1.${Font_color_suffix} 阿里源
- ${Green_font_prefix}2.${Font_color_suffix} 腾讯源 
- ${Green_font_prefix}3.${Font_color_suffix} 清华源
- ${Green_font_prefix}4.${Font_color_suffix} 网易源
- ${Green_font_prefix}5.${Font_color_suffix} 自动更换源
- ${Green_font_prefix}6.${Font_color_suffix} 退出
- ---------------------------------------------------------" && echo -e "系统:${ID}版本:${VERSION_ID}"
- ${gen} main
- case "$main" in
-	 1)
-	yuan="ali"
-	oso="-f"
-	yt="mirrors.aliyun.com"
-	h="http"
-	syst
-	 ;;
- 	 2)
-       yuan="tx"
-       oso="-f"
-       yt="mirrors.cloud.tencent.com"
-       h="https"
-       syst
-         ;;
-	 3)
-       yuan="qh"
-       oso="-f"
-       yt="mirrors.tuna.tsinghua.edu.cn"
-       h="https"
-       syst
-	 ;;
- 	 4)
-       yuan="wy"
-       oso="-f"
-       yt="mirrors.163.com"
-       h="http"
-       syst
- 	 ;;
-	 5)
-	automation
-	 ;;
-	 6)
-	clear
-	exit 1
-         ;;
-         *)
-         clear
-	 echo -e "${Error} ${none}"
-	 sleep 1s
-	 menu
-	 ;;
- esac
-}
-
-error(){
-echo -e "开发中"
-
-
-}
-#系统选择
-syst(){
+${Green_font_prefix}0.${Font_color_suffix}升级脚本
+--------------------选择系统源----------------------------
+${Green_font_prefix}1.${Font_color_suffix} 在线
+${Green_font_prefix}2.${Font_color_suffix} 离线
+${Green_font_prefix}3.${Font_color_suffix} 退出
+---------------------------------------------------------" && echo -e "系统:${ID}版本:${VERSION_ID}"
+${gen} main
+case "$main" in
+0)
+Update_shell
+;;
+1)
+online
+;;
+2)
+offline
+;;
+;;
+3)
+exit 1
+;;
+*)
 clear
-echo -e "${muban}"
- ${gen} os
-case "$os" in
-        1)
-	sys="ubuntu"
-	fl="/etc/apt/sources.list"
-	bc=".bak"
-	update="apt-get update"
-	ub
-	;;
-	2)
-	sys="centos"
-	fl="/etc/yum.repos.d/CentOS-Base.repo"
-	bc=".backup"
-	update="yum makecache && yum update"
-	ce
-	;;
-        3)	
-	sys="debian"
-	fl="/etc/apt/sources.list"
-	bc=".bak"
-	update="apt-get update"
-	sudo apt install apt-transport-https ca-certificates
-	de
-	;;
-   	4)
-	update="sudo zypper ref && zypper update"
-	op
-	;;
-	5)
-	sys="fedora"
-	fl="/etc/yum.repos.d/fedora.repo"
-	fl2="/etc/yum.repos.d/fedora-updates.repo"
-	bc=".backup"
-	ver="1.repo"
-	ver2="2.repo"
-	update="yum makecache && yum update"
-	run
-	run2
-       	;;
-	6)
-	fl="/etc/pacman.d/mirrorlist"
-	update="pacman -Syy"
-	you1
-	;;
-	7)
-	bc=".bak"
-	sys="kali"
-	fl="/etc/apt/sources.list"
-	update="apt-get update"
-	ver="1.list"
-	run
-	;;
-	8)
-	bc=".bak"
-	sys="deepin"
-	update="apt-get update"
-	ver="1.list"
-	fl="/etc/apt/sources.list"
-	run
-	;;
-	9)
-	bc=".bak"
-	sys="alpine"
-	update="apk update"
-	fl="/etc/apk/repositories"
-	alpines
-	;;
-	10)
-	menu
-	;;
-	*)
-	clear
-	 echo -e "${Error} ${none}"
-	 sleep 1s
-	 syst
-	 ;;
-	 
- esac
+echo -e "${Error} ${none}"
+sleep 1s
+allelect
+;;
+esac
+}
+#在线主菜单
+online(){
+clear
+echo -e "${tongyong}"
+${gen} onl
+case "onl" in
+1)
+yuan="ali"
+oso="-f"
+yt="mirrors.aliyun.com"
+h="http"
+onautomation
+;;
+2)
+yuan="tx"
+oso="-f"
+yt="mirrors.cloud.tencent.com"
+h="https"
+onautomation
+;;
+3)
+yuan="qh"
+oso="-f"
+yt="mirrors.tuna.tsinghua.edu.cn"
+h="https"
+onautomation
+;;
+4)
+yuan="wy"
+oso="-f"
+yt="mirrors.163.com"
+h="http"
+onautomation
+;;
+5)
+clear
+exit 1
+;;
+*)
+clear
+echo -e "${Error} ${none}"
+sleep 1s
+online
+;;
+esac
 }
 
-
-
-#ubuntu选择菜单
-ub(){
-clear
-echo -e "${ubuntu1}"
- ${gen} num
- case "$num" in
-	1)
-	ver="20.04.list"
-        run
-	;;
-	2)
-	ver="18.04.list"
-        run
-	;;
-        3)
-	ver="16.04.list"
-	run
-	;;
-   	4)
-	ver="14.04.list"
-	run
-	;;
-	5)
-	#aliubuntu
-       	;;
-	6)
-	#aliubuntu
-	;;
-	7)
-	#aliubuntu
-	;;
-	8)
-	#aliubuntu
-	;;
-	9)
-	syst
-	;;
-	*)
-	clear
-	 echo -e "${Error} ${none}"
-	 sleep 1s
-	 ub
-	 ;;
-	 
- esac
-}
-#debian选择菜单
-de(){
-clear
-echo -e "${debian1}"
- ${gen} num
- case "$num" in
-	1)
-	ver="9.list"
-       run
-	;;
-	2)
-	ver="8.list"
-       run
-	;;
-        3)
-	ver="7.list"
-       run	
-	;;
-   	4)
-    	;;
-	5)
-	
-       	;;
-	6)
-	
-	;;
-	7)
-	
-	;;
-	8)
-	
-	;;
-	9)
-	syst
-	;;
-	*)
-	clear
-	 echo -e "${Error} ${none}"
-	 sleep 1s
-	 de
-	 ;;
-	 
- esac
-}
-#centos选择菜单
-ce(){
-clear
-echo -e "${centos1}"
- ${gen} num
- case "$num" in
-	1)
-	ver="8.repo"
-       run
-	;;
-	2)
-	ver="7.repo"
-       run
-	;;
-        3)
-	ver="6.repo"
-       run	
-	;;
-   	4)
-       run
-	;;
-	5)
-	
-       	;;
-	6)
-	
-	;;
-	7)
-	
-	;;
-	8)
-	
-	;;
-	9)
-	syst
-	;;
-	*)
-	clear
-	 echo -e "${Error} ${none}"
-	 sleep 1s
-	 ce
-	 ;;
-	 
- esac
-}
-#alpine使用
-alpines(){
-mc ${fl}.bak ${fl}
-cp ${fl} ${fl}.bak
-sed -i 's/dl-cdn.alpinelinux.org/'"${yt}"'/g' ${fl}
-${update}
-}
-alpine1(){
-clear
-echo -e "${alpine}"
- ${gen} num
- case "$num" in
-	0)
-	ver="2.4"
-        run
-	;;
-	1)
-	ver="2.5"
-        run
-	;;
-        2)
-	ver="2.6"
-        run	
-	;;
-   	3)
-	ver="2.7"
-	run
-    	;;
-	4)
-	ver="3.0"
-	run
-       	;;
-	5)
-	ver="3.1"
-	run
-	;;
-	6)
-	ver="3.10"
-	run
-	;;
-	7)
-	ver="3.11"
-	run
-	;;
-	8)
-	ver="3.2"
-	run
-	;;	
-	9)
-	ver="3.3"
-       run
-	;;
-	10)
-	ver="3.4"
-       run
-	;;
-        11)
-	ver="3.5"
-       run	
-	;;
-   	12)
-	ver="3.6"
-	run
-    	;;
-	13)
-	ver="3.7"
-	run
-       	;;
-	14)
-	ver="3.8"
-	run
-	;;
-	15)
-	ver="3.9"
-	run
-	;;
-	*)
-	clear
-	 echo -e "${Error} ${none}"
-	 sleep 1s
-	 alpine1
-	 ;;
-	 
- esac
-}
-#y皆为opensuse任务处理
-op(){
-echo -e "${openSUSE1}"
-${gen} jieguo
-nums=`echo ${jieguo} |sed 's/[-0-9]//g'`
-if [ ! -z ${nums} ]
-then
-echo -e "请输入纯数字"
+  #以下结为自动化
+  #ubuntu
+onautomation(){
+if [ "${ID}" = "ubuntu" ];then
+sys="${ID}"
+ver="${VERSION_ID}.list"
+bc=".bak"
+fl="/etc/apt/sources.list"
+update="sudo apt-get update"
+run
 else
-guodu
-fi
-
-}
-
-guodu(){
-if [ "${jieguo}" == "15" ];then
- ve="/leap/"
- jixua
-else
-zailai 
+oncentos
 fi
 }
-zailai(){
-if [ "${jieguo}" == "42" ];then
- ve="/leap/"
- jixua
+#centos
+oncentos(){
+if [ "${ID}" = "centos" ];then
+update="sudo yum makecache && yum update"
+oncentos7
+fi
 else
-jixua
+ondebian
+fi
+}
+oncentos7(){
+if [ "${VERSION_ID}" = "7" ];then
+sys=${ID}
+ver="${VERSION_ID}.repo"
+bc=".bakup"
+fl="/etc/yum.repos.d/CentOS-Base.repo"
+else
+oncentos6
+fi
+}
+oncentos6(){
+if [ "${VERSION_ID}" = "6" ];then
+sys=${ID}
+ver="${VERSION_ID}.repo"
+bc=".bakup"
+fl="/etc/yum.repos.d/CentOS-Base.repo"
+else
+else
+oncentos8
+fi
+}
+oncentos8(){
+if [ "${VERSION_ID}" = "8" ];then
+sys=${ID}
+ver="${VERSION_ID}.repo"
+bc=".bakup"
+fl="/etc/yum.repos.d/CentOS-Base.repo"
+else
+clear
+echo -e "CentOS低版本暂时懒得写"
+fi
+  }
+  #debian
+ondebian(){
+if [ "${ID}" = "debian" ];then
+sys="${ID}"
+ver="${VERSION_ID}.list"
+bc=".bak"
+fl="/etc/apt/sources.list"
+update="sudo apt-get update"
+run
+else
+onfedora
+fi
+}
+#fedora
+onfedora(){
+if [ "${ID}" = "fedora" ];then
+sys="${ID}"
+fl="/etc/yum.repos.d/fedora.repo"
+fl2="/etc/yum.repos.d/fedora-updates.repo"
+bc=".backup"
+ver="1.repo"
+ver2="2.repo"
+update="yum makecache && yum update"
+else
+onkali
 fi
 }
 
-jixua(){
-echo -e "${opensus}"
-${gen} meiyong
- case "$meiyong" in
-	1)
-	vers="${jieguo}.1"
-       osu
-	;;
-	2)
-	vers="${jieguo}.2"
-	osu
-	;;
-        3)
-	vers="${jieguo}.3"
-	osu	
-	;;
-   	4)
-	vers="${jieguo}.4"
-	osu
-	;;
-	5)
-	vers="${jieguo}.5"
-	osu
-	;;
-	*)
-	clear
-	 echo -e "${Error} ${none}"
-	 sleep 1s
-	 jixua
-	 ;;
-	 
- esac
+onkali(){
+if [ "${ID}" = "kali" ];then
+sys="${ID}"
+ver="1.list"
+bc=".bak"
+fl="/etc/apt/sources.list"
+update="apt-get update"
+run
+else
+clear
+echo -e "剩下的只有离线版本啦"
+fi
 }
-#arch任务处理
-you1(){
-cp ${fl} ${fl}.backup
-sed -i '1 i\Server = '"${h}"'://'"${yt}"'/archlinux/$repo/os/$arch' ${fl}
-${update}
-echo -e "完成"
-sleep 3s
-}
-#deepin任务处理
-depin(){
-cp ${fl} ${fl}.bak
-sed -i '1 i\deb [by-hash=force] '"${h}"'://'"${yt}"'/deepin lion main contrib non-free' ${fl}
-${update}
-echo -e "完成"
-sleep 3s
-}
-
-#opensuse任务处理
-osu(){
-echo -e "${ve}${vers}"
-sudo zypper mr -da
-sduo zypper rr rpos
-sudo zypper rr rpnos
-sudo zypper rr uposs
-duo zypper rr upnos
-sudo zypper addrepo ${oso} ${h}://${yt}/opensuse/distribution${ve}${vers}/repo/oss/ rpos
-sudo zypper addrepo ${oso} ${h}://${yt}/opensuse/distribution${ve}${vers}/repo/non-oss/ rpnos
-sudo zypper addrepo ${oso} ${h}://${yt}/opensuse/update${ve}${vers}/oss/ uposs
-sudo zypper addrepo ${oso} ${h}://${yt}/opensuse/update${ve}${vers}/non-oss/ upnos
-${update}
-echo -e "完成"
-sleep 3s
-}
-#r基本任务处理
+#r基本任务处理,本不想使用这么蠢的结构不过我好懒啊
 run(){
 echo -e "${fl}${dl}${yuan}${sys}${ver}${bc}"
 echo -e "${yuan}${sys}${ver}"
@@ -861,7 +575,7 @@ echo -e "更换完成"
 sleep 3s
 fi
 }
-
+#我懒了直接复制修改吧
 run2(){
 echo -e "${fl2}${dl}${yuan}${sys}${ver2}${bc}"
 echo -e "${yuan}${sys}${ver2}"
@@ -902,8 +616,3 @@ echo -e "更换完成"
 sleep 3s
 fi
 }
-
-menu
-
-
-
